47f74db5c2f743e6847222c065a319efd1009167
MohakGupta2004 <mohakgupta500@gmail.com>
2024-12-03 10:17:45 +0530

    Increase buffer cache from 5 mins to 15 mins

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -18,7 +18,7 @@ const tokenCache = new LRUCache<string, DecodedIdToken>({
     JSON.stringify(token).length + key.length, //sizeInBytes
 });
 
-const TOKEN_CACHE_BUFFER = 1000 * 60 * 5; // 5 minutes
+const TOKEN_CACHE_BUFFER = 1000 * 60 * 15; // 15 minutes instead of 5
 
 export async function verifyIdToken(
   idToken: string,
